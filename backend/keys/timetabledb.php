707e765f5c94a0ad274f955e5e09de15c5bd583e
DerekY2 <derek.yu869@gmail.com>
2025-03-18 12:54:25 -0400

    0.3.0 AWS Playgorund

---
+++ backend/keys/timetabledb.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_SERVER', 'database-1.c3imay6w07q1.ca-central-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'rdstest869');
+define('DB_DATABASE', 'timetabledb');
+?>
